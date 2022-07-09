3532ff3fe591dd96190093af0417c2d889fb8d29
MikeKondr99@gmail.com <MikeKondr99@gmail.com>
2022-07-09 19:10:26 +0300

    Add project files.

---
+++ PizzaStore/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PizzaStore.Models;
+
+namespace PizzaStore.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class HomeController : Controller
+    {
+        [HttpGet("")]
+        public IActionResult SayHello()
+        {
+            return View("StringView", new StringModel("Hello Tomek!"));
+        }
+
+        [HttpGet("kawabanga")]
+        public IActionResult GetPizzas()
+        {
+            return View("Pizzas", new List<Pizza>()
+            {
+                new Pizza("Маргарита"),
+                new Pizza("Пеперони"),
+                new Pizza("Мясная"),
+            });
+        }
+
+
+    }
+}
