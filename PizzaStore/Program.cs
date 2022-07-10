44dd062467459935107d4dafdbdb58763e95eb6e
MikeKondr99@gmail.com <MikeKondr99@gmail.com>
2022-07-10 22:32:51 +0300

    добавил тестовый сервис для продуктов

--- PizzaStore/Program.cs
+++ PizzaStore/Program.cs
@@ -1,7 +1,12 @@
+using PizzaStore.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
+var services = builder.Services;
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+services.AddControllersWithViews();
+services.AddSingleton<IProductService, ProductListService>();
+
 
 var app = builder.Build();
 
